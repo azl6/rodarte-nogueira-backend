77f26723b06284931e54e4b84d74fb65b7389288
Alex <alex.rodrigues23997@gmail.com>
2022-05-25 20:43:06 -0300

    Initial commit

---
+++ src/main/java/com/azold6/rodartenogueirabackend/RodarteNogueiraBackendApplication.java
@@ -0,0 +1,13 @@
+package com.azold6.rodartenogueirabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RodarteNogueiraBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RodarteNogueiraBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azold6/rodartenogueirabackend/RodarteNogueiraBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azold6.rodartenogueirabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RodarteNogueiraBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
